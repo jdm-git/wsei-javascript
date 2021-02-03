31ff7a43d776eba2f2bc7d03948987312b070c7a
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-02-03 12:51:20 +0100

    15. Finished

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -87,10 +87,8 @@ document.body.append(secondDiv, r00t);
 //7
 const dataTable = ['abc', 'efg', 'hij', 'klm','nop','rst'];
 const firstOlList = document.createElement('ol');
-firstOlList.id = 'firstList';
 
 const firstBtn = document.createElement('button');
-firstBtn.id = 'firstBtn';
 firstBtn.innerText = 'First list';
 
 dataTable.forEach((e) => {
@@ -101,10 +99,8 @@ dataTable.forEach((e) => {
 });
 
 const secondOlList = document.createElement('ol');
-secondOlList.id = 'secondList';
 
 const secondBtn = document.createElement('button');
-secondBtn.id = 'secondBtn';
 secondBtn.innerText = 'Second list';
 secondBtn.disabled = true;
 
@@ -112,18 +108,322 @@ root.appendChild(firstOlList);
 root.appendChild(firstBtn);
 root.appendChild(secondOlList);
 root.appendChild(secondBtn);
+let li = document.createElement('li');
 
-const transferLiElementToSecondList = (firstOlList, secondOlList) => {
-    let firstList = firstOlList.getElementsByTagName('li');
-    let secondList = secondOlList.getElementsByTagName('li');
-    let lastElem = firstList[firstList.length - 1];
-    let newElement = document.createElement('li');
-    newElement = lastElem.innerText;
 
-    secondList.appendChild(newElement);
-    firstOlList.removeChild(lastElem);
+const transferLiElementToSecondList = () => {
 
+  let firstLis = firstOlList.getElementsByTagName('li');
+  let lastLi = firstLis[firstLis.length - 1];
+  secondOlList.appendChild(lastLi);
+  
+  if(secondOlList.getElementsByTagName('li').length > 0)
+    secondBtn.disabled = false;
+
+  if(firstOlList.getElementsByTagName('li').length < 1)
+    firstBtn.disabled = true;
 }
 
-firstBtn.addEventListener('click', transferLiElementToSecondList(firstOlList, secondOlList));
+const transferLiElementToFirstList = () => {
+  let secondLis = secondOlList.getElementsByTagName('li');
+  let lastLi = secondLis[secondLis.length - 1];
+  firstOlList.appendChild(lastLi);
 
+  if(firstOlList.getElementsByTagName('li').length > 0)
+    firstBtn.disabled = false;
+  if(secondOlList.getElementsByTagName('li').length < 1)
+    secondBtn.disabled = true;
+}
+
+firstBtn.addEventListener("click",transferLiElementToSecondList);
+secondBtn.addEventListener('click', transferLiElementToFirstList);
+
+
+//8
+
+// const fieldset = document.createElement('fieldset');
+// const inputs = [{
+//     label: 'Element',
+//     id: 'el',
+//     type: 'text'
+// }, {
+//     label: 'Tekst',
+//     id: 'text',
+//     type: 'text'
+// }, {
+//     label: 'Kolor',
+//     id: 'color',
+//     type: 'color'
+// }, {
+//     label: 'Ile razy',
+//     id: 'count',
+//     type: 'number'
+// }, {
+//     label: 'Utwórz',
+//     type: 'submit'
+// }];
+
+// inputs.forEach(v => {
+//     const elInput = document.createElement('input');
+//     let label = document.createElement('hr');
+//     elInput.style.display = 'block';
+//     elInput.type = v.type;
+//     elInput.id = v.id || null;
+
+//     if (v.type === 'submit') {
+//         elInput.value = v.label;
+//         elInput.addEventListener('click', (e) => {
+//             createElement(e);
+//         });
+//     } else {
+//         label = document.createElement('label');
+//         label.innerText = v.label;
+//         label.for = v.id;
+//     }
+//     fieldset.appendChild(label);
+
+//     fieldset.appendChild(elInput);
+// });
+
+// function createElement(e) {
+//     e.preventDefault();
+//     let el = null;
+
+//     inputs.forEach((v) => {
+//         const value = document.getElementById(v.id)?.value;
+
+//         switch (v.id) {
+//             case 'el': el = document.createElement(value); break;
+//             case 'text': el.innerText = value; break;
+//             case 'color': el.style.color = value; break;
+//             case 'count': for (let i = 1; i <= Number(value); i++) {
+//                 document.body.appendChild(el.cloneNode(true));
+//             } break;
+//         }
+//     });
+// }
+
+// document.body.appendChild(fieldset);
+
+
+//9
+
+
+// const fieldset = document.createElement('fieldset');
+// const table = document.createElement('table');
+
+// const inputs = [{
+//     label: 'Imię',
+//     id: 'fName',
+//     type: 'text'
+// }, {
+//     label: 'Nazwisko',
+//     id: 'name',
+//     type: 'text'
+// }, {
+//     label: 'Wiek',
+//     id: 'age',
+//     type: 'number'
+// }, {
+//     label: 'Ilość dzieci',
+//     id: 'kids',
+//     type: 'number'
+// }, {
+//     label: 'Płeć',
+//     id: 'gender',
+//     type: 'text'
+// }, {
+//     label: 'Więcej',
+//     type: 'submit',
+//     id: 'more'
+// }, {
+//     label: 'Utwórz',
+//     type: 'submit',
+//     id: 'print'
+// }];
+
+// let data = [];
+
+// inputs.forEach(v => {
+//     const elInput = document.createElement('input');
+//     let label = document.createElement('hr');
+//     elInput.style.display = 'block';
+//     elInput.type = v.type;
+//     elInput.id = v.id || null;
+
+//     if (v.type === 'submit') {
+//         elInput.value = v.label;
+//         elInput.addEventListener('click', (e) => {
+//             if (v.id === 'print') {
+//                 renderTable(e);
+//             } else {
+//                 addRow();
+//             }
+//         });
+//     } else {
+//         label = document.createElement('label');
+//         label.innerText = v.label;
+//         label.for = v.id;
+//     }
+//     fieldset.appendChild(label);
+
+//     fieldset.appendChild(elInput);
+// });
+
+// function addRow() {
+//     const row = [];
+//     inputs.forEach((v) => {
+//         if (v.type !== 'submit') {
+//             const value = document.getElementById(v.id)?.value;
+//             row.push(value);
+//         }
+//     });
+//     data.push(row);
+    
+//     if (table.children.length > 0) {
+//         renderTable();
+//     }
+// }
+
+// function renderTable(e) {
+//     e?.preventDefault();
+//     table.innerHTML = '';
+//     if (data.length > 0) {
+//         const dataCells = inputs.filter(v => v.type !== 'submit');
+        
+//         const thead = document.createElement('thead');
+//         dataCells.forEach(v => {
+//             const th = document.createElement('th');
+//             th.innerText = v.label;
+//             thead.appendChild(th);
+//         });
+//         table.appendChild(thead);
+       
+//         const tbody = document.createElement('tbody');
+//         capitalize();
+//         data.forEach((v, i) => {
+//             const tr = document.createElement('tr');
+//             v.forEach(val => {
+//                 const td = document.createElement('td');
+//                 td.innerText = val;
+//                 tr.appendChild(td);
+//             })
+//             const removeButton = document.createElement('button');
+//             removeButton.innerText = 'Usuń';
+//             removeButton.addEventListener('click', (e) => {
+//                 data.splice(i, 1);
+//                 renderTable();
+//             });
+//             tr.appendChild(removeButton);
+//             tbody.appendChild(tr);
+//         });
+//         table.appendChild(tbody);
+//     }
+// }
+
+// document.body.appendChild(fieldset);
+// document.body.appendChild(table);
+
+// //10
+
+// function capitalize() {
+//   data = data.map(dat =>
+//       dat.map(v => v.charAt(0).toUpperCase() + v.slice(1))
+//   );
+// }
+
+// //11
+// function extarctNumbersAndMultiplyToDivs(str) {
+//   const numbers = str.match(/[0-9]+/g);
+//   if (numbers.length > 0) {
+//       console.log(numbers.reduce((a, b) => Number(a) + Number(b)));
+
+//       const mmultiplier = numbers.reduce((a, b) => Number(a) * Number(b));
+
+//       for (let i = 0; i < mmultiplier; i++) {
+//           const div = document.createElement('div');
+//           div.innerText = `div${i}`;
+//           document.body.appendChild(div);
+//       }
+//   }
+// }
+
+// extarctNumbersAndMultiplyToDivs('foo1bar2test10nice2');
+
+//12
+
+function Ala(str){
+  this.string = str;
+  this.checkAla = () =>{
+    if(this.string.includes('Ala'))
+      this.string = this.string.replaceAll('Ala', 'Ola');
+    else{
+      const div = document.createElement('div');
+      div.innerText = "'Ala' nie wystepuje w tekscie";
+      root.appendChild(div);
+    }
+  }
+}
+const ala = new Ala("Ala Ma Kota");
+ala.checkAla();
+
+
+//13
+
+
+const dataArray = ['Lorem', 'L0r3m', 'g8 b8 m8', 'foo2bar4ever'];
+function returnHowManyLetters(arr) {
+    let numberCounter = 0;
+    arr.forEach(v => {
+        const str = v.replace(/[^a-z]/gi, '');
+        numberCounter += str.length;
+    });
+    return numberCounter;
+}
+
+function sumNumbers(arr) {
+    const numbers = arr.map(v => v.match(/[0-9]+/g));
+    const allNumbers = numbers.flat();
+    return allNumbers.reduce((a, b) => Number(a) + Number(b));
+}
+
+function avgNumbers(arr) {
+    const numbers = arr.map(v => v.match(/[0-9]+/g));
+    const allNumbers = numbers.flat().filter(v => v);
+    const sum = allNumbers.reduce((a, b) => Number(a) + Number(b));
+    return sum / allNumbers.length;
+}
+
+console.log(returnHowManyLetters(dataArray));
+console.log(sumNumbers(dataArray));
+console.log(avgNumbers(dataArray));
+
+
+//14
+
+const obj = {
+  name: '',
+  surname: '',
+  age: ''
+}
+console.log(obj);
+const modifyObj = (obj, newName, newSurname, newAge) => {
+  obj.name = newName,
+  obj.surname = newSurname,
+  obj.age = newAge
+  console.log(obj);
+  if(obj.name.length > 5 || obj.surname.length > 5){
+    const btn = document.createElement('button');
+    btn.innerText = "Restore props";
+    btn.addEventListener('click', () => {
+      obj.name =  '';
+      obj.surname = '';
+      obj.age =  '';
+      console.log(obj);
+    })
+    root.appendChild(btn);
+    
+    
+  }
+}
+modifyObj(obj, 'Kubaaaaa', 'Meus',59);
